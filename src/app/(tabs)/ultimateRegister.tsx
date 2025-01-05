4d4e224c6fdc6940e7b28ee5fe293c284eb379aa
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-01-05 19:25:57 -0300

    feat: remoção de borderRadius em itemContainer

--- src/app/(tabs)/ultimateRegister.tsx
+++ src/app/(tabs)/ultimateRegister.tsx
@@ -208,7 +208,7 @@ const styles = StyleSheet.create({
    itemContainer: {
       padding: 10,
       marginBottom: 10,
-      borderRadius: 5,
+     
       borderWidth: 1,
       borderColor: '#ccc',
       backgroundColor: '#fff'
